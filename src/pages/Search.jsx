79d3ef615006f07b56faeb9dd789f3d24ee56bdd
juliana <julianamq20@gmail.com>
2022-08-05 09:56:57 -0300

    atualizando

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,73 +1,75 @@
 import React from 'react';
 import Header from '../Components/Header';
-import searchAlbumsAPI from '../services/searchAlbumsAPI';
-// import Loading from '../Components/Loading';
-// import Album from './Album';
+// import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       search: '',
-
-      // loading: false,
-      // albuns: [],
+      // albumArtist: [],
       // artist: '',
+      // loading: false,
     };
   }
 
-  onInputChange = ({ target }) => {
-    this.setState({
-      search: target.value,
-    });
-  }
-
-  // async onButtonClick() {
-  //   const { search } = this.state;
-  //   this.setState({
-  //     search: '',
-  //     // loading: true,
-  //   });
-  //   const albuns = await searchAlbumsAPI(search);
-  //   this.setState({
-  //     albuns,
-  //     // loading: false,
-  //     // artist: search,
-  //   });
-  // }
-
-  render() {
-    const { search,
-      // loading,
-      //  albuns,
-      //  artist
-    } = this.state;
-
-    return (
-      <div data-testid="page-search">
-        <div>
-          <Header />
-          <input
-            id="search"
-            data-testid="search-artist-input"
-            type="text"
-            name="search"
-            value={ search }
-            onChange={ this.onInputChange }
-          />
-          <button
-            data-testid="search-artist-button"
-            type="button"
-            disabled={ search.length < 2 }
-            onClick={ this.onButtonClick }
-          >
-            Pesquisar
-          </button>
-
-        </div>
-      </div>
-    );
-  }
+onInputChange = ({ target }) => {
+  this.setState({
+    search: target.value,
+  });
 }
 
+// para pesquisar o artista quando clicar no botao
+// onButtonClick = async () => {
+//   const { search } = this.state;
+//   this.setState({
+//     search: '',
+//     loading: true,
+//   });
+//   const albuns = await searchAlbumsAPI(search).map((album) => {
+//     album.artist = albumArtist;
+//   });
+// }
+
+// // para limpar o input
+// handleReset = () => {
+//   this.setState({
+//     artist: [{}],
+//   });
+// };
+
+render() {
+  const {
+    search,
+    // loading,
+    // albumArtist,
+    // artist
+  } = this.state;
+
+  return (
+    <div data-testid="page-search">
+      <div>
+        <Header />
+
+        <input
+          id="search"
+          data-testid="search-artist-input"
+          type="text"
+          name="search"
+          value={ search }
+          onChange={ this.onInputChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ search.length < 2 }
+          onClick={ this.onButtonClick }
+        >
+          Pesquisar
+        </button>
+      </div>
+    </div>
+  );
+}
+}
 export default Search;
